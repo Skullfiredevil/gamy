99c1397ed3e24362e73b58fc550ecc45ffebfa60
Skullfiredevil <jotipinto@gmail.com>
2023-12-01 15:40:13 +0000

    damn

--- db/schema.rb
+++ db/schema.rb
@@ -54,13 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_103415) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
